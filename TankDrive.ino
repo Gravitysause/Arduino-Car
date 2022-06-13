3a80c1ab6521ed3d6e59b226a9f85e405e04fa62
Gravitysause <57376726+Gravitysause@users.noreply.github.com>
2022-06-13 14:27:29 -0400

    Adding the tank drive code
    This was made in tinkercad, so I'm moving it to the repo.

---
+++ TankDrive.ino
@@ -0,0 +1,66 @@
+#define rightside 2
+#define leftside 3
+
+#define rightForward 5
+#define rightReverse 4
+
+#define leftForward 7
+#define leftReverse 6
+
+
+void setup() {
+  for(int pin = 2; pin <= 9; pin++) {
+    pinMode(pin, OUTPUT);
+  }
+}
+
+
+void moveForward() {
+  digitalWrite(rightReverse, LOW);
+  digitalWrite(leftReverse, LOW);
+  
+  digitalWrite(rightForward, HIGH);
+  digitalWrite(leftForward, HIGH);
+}
+
+
+void moveBackwards() {
+  digitalWrite(rightForward, LOW);
+  digitalWrite(leftForward, LOW);
+  
+  digitalWrite(rightReverse, HIGH);
+  digitalWrite(leftReverse, HIGH);
+}
+
+
+void moveRight() {
+  digitalWrite(rightReverse, LOW);
+  digitalWrite(leftForward, LOW);
+  
+  digitalWrite(rightForward, HIGH);
+  digitalWrite(leftReverse, HIGH);
+}
+
+
+void moveLeft() {
+  digitalWrite(rightForward, LOW);
+  digitalWrite(leftReverse, LOW);
+  
+  digitalWrite(rightReverse, HIGH);
+  digitalWrite(leftForward, HIGH);
+}
+
+
+void loop() {
+  digitalWrite(rightside, HIGH);
+  digitalWrite(leftside, HIGH);
+  
+  moveForward();
+  delay(400);
+  moveBackwards();
+  delay(400);
+  moveRight();
+  delay(400);
+  moveLeft();
+  delay(400);
+}
